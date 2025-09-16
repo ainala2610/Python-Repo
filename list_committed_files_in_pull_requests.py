20a73ee8ae8e0b1ee2768937b29176c9b8670595
ainala2610 <ajay.in2610@gmail.com>
2025-09-16 14:05:22 +0530

    Create list_committed_files_in_pull_requests.py

---
+++ list_committed_files_in_pull_requests.py
@@ -0,0 +1,58 @@
+
+
+# Program to demonstrate integration with GitHub to fetch the 
+# details of Users who created Pull requests(Active) on Kubernetes Github repo.
+
+import requests
+
+# URL to fetch pull requests from the GitHub API
+url = f'https://api.github.com/repos/ainala2610/Python-Repo/pulls'
+
+token = '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'
+headers = {
+    'Authorization': f'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'                    # Provide the toke here
+    'Accept' 'application/vnd.github.v3+json'
+}
+
+# Make a GET request to fetch pull requests data from the GitHub API
+response = requests.get(url, headers=headers)  # Add headers=headers inside get() for authentication
+
+# Only if the response is successful
+if response.status_code == 200:
+    # Convert the JSON response to a dictionary
+    pull_requests = response.json()
+
+    # Create an empty dictionary to store PR creators and their counts
+    #pr_creators = {}
+    pull_requests_list = {}
+
+    for pullid in pull_requests:
+      pullrequest_id = pullid['number']
+      print(pullrequest_id)
+      if pullrequest_id in pull_requests_list:
+          pull_requests_list[pullrequest_id] += 1
+             #print(pull_requests_list)
+      else:
+        pull_requests_list[pullrequest_id] = 1
+
+
+    print (pull_requests_list)
+    print("Formated pull requests_id's:",list(pull_requests_list.keys()))
+
+    count =  pull_requests_list.items()
+    print("Total Number of pull requests:", len(pull_requests_list))
+
+    for number in pull_requests_list:
+        url2 = f'https://api.github.com/repos/ainala2610/Python-Repo/pulls/{number}'
+        url3 = f'https://api.github.com/repos/ainala2610/Python-Repo/pulls/{number}/files'
+        response1 = requests.get(url2)
+        response2 = requests.get(url3)
+        data2 = response1.json()
+        data3 = response2.json()
+        pullrequest_owner = data2["user"]["login"]
+        filename = data3[0]['filename']
+        print(filename)
+        print(f"The Pull Request {number} has been reaised by: {pullrequest_owner} and the committed files: {filename}")
+
+else:
+    print(f"Failed to fetch data. Status code: {response.status_code}")
