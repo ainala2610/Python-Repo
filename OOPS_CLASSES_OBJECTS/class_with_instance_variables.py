f4f7639f15408e0fa3e8edd998042b6987b2a00c
ainala2610 <ajay.in2610@gmail.com>
2025-09-18 12:32:27 +0530

    Create class_with_instance_variables.py

---
+++ OOPS_CLASSES_OBJECTS/class_with_instance_variables.py
@@ -0,0 +1,7 @@
+## Instance variable 
+class dog:
+     
+## Defining constructor
+     def __init__(self,name,age):
+            self.NAME=name   #self.NAME is a variable which is assigned to name
+            self.age=age
